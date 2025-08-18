e186421396d1fd651b0f209c7a01d45682cc4976
Avichal Pandey <avichal2018lko@gmail.com>
2025-08-19 01:08:15 +0530

    ui change

--- frontend/src/pages/gemini.jsx
+++ frontend/src/pages/gemini.jsx
@@ -41,8 +41,8 @@ export default function Gemini(){
   };
 
     return(
-        <div className='bg-black/40 h-[95vh] w-full max-w-4xl relative shadow shadow-white rounded-2xl px-4 pt-2 items-center'>
-            <div className='h-[80%] overflow-y-auto relative overflow-x-hidden flex flex-col gap-4 px-4 border border-b-gray-300'>
+        <div className='bg-black/40 h-[95vh] w-[98%] max-w-4xl relative shadow shadow-white rounded-2xl px-4 pt-2 items-center'>
+            <div className='h-[80%] overflow-y-auto relative overflow-x-hidden flex flex-col gap-4 px-4 border border-b-white'>
             {prompt&&<div className='bg-gray-800 break-all [overflow-wrap:anywhere] w-fit flex self-end p-4 rounded-4xl text-white'>{prompt}</div>}
             {loading&&<div className='self-center absolute top-[50%] right-[55%]'><Loader/></div>}
             {response&&<div className='bg-gray-800 break-all [overflow-wrap:anywhere] w-fit max-w-[95%] flex p-4 rounded-4xl text-white'>{response}</div>}
