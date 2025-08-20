198d31549ee34ce5dfd237ef987c375473836dc8
Avichal Pandey <avichal2018lko@gmail.com>
2025-08-20 23:20:05 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ import mainRoute from './routes/index.js';
 const app=express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "https://lamina-ai-avi.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use('/api/v1',mainRoute);
 
 app.get('/ping',(req,res)=>{
